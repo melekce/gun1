9695ef0fae87b65c0df8386189d7bf61c3613983
melekce <melekce@melekce-macbook-air.local>
2016-02-20 10:30:27 +0200

    Initial Commit

---
+++ SwiftGun1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftGun1
+//
+//  Created by melekce on 20.02.2016.
+//  Copyright © 2016 melekce. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
